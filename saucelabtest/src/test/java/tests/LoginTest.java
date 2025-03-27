642f8497a438b0861469c92e1255d53cfc2fd8f1
deaack <deaackben66@gmail.com>
2025-03-27 13:07:37 +0100

    login test

---
+++ saucelabtest/src/test/java/tests/LoginTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pageObjects.LoginPage;
+import utils.Credential;
+import utils.CredentialReader;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.firefoxdriver().setup(); //automatic Webdriver handle
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testLoginWithValidCredentials() {
+        Credential credential = CredentialReader.readCredential();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openLoginPage();
+        loginPage.login(credential.getUsername(), credential.getPassword());
+
+        assertTrue(driver.getCurrentUrl().contains("inventory"), "Successful login.");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // if (driver != null) {
+        //     driver.quit();
+        // }
+    }
+}
